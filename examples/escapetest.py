ae0c645a2bbf89d2da037d81c15bfad5ac339086
shrkey <barry@shrkey.com>
2016-04-02 18:51:28 +0100

    quick example update for now

--- examples/escapetest.py
+++ examples/escapetest.py
@@ -1,7 +1,7 @@
 import time
-from dw640HAT import dw_MotorCONTROL, dw_DCMotor
+from dwescapeHAT import dw_PWMCONTROL, dw_PWM
 
 dw = dw_PWMCONTROL( addr=0x61 )
-m = dw.getMotor(2)
+s = dw.getSERVO(1)
 
-m.off();
+s.off();
