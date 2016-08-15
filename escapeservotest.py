67a760903cb1ad60d6f605b5ca6aedcb85de9bd7
shrkey <barry@shrkey.com>
2016-08-15 21:00:58 +0100

    library change

--- escapeservotest.py
+++ escapeservotest.py
@@ -1,5 +1,5 @@
 import time
-from darkwater_640.darkwater_640 import dw_Controller, dw_Servo, dw_Motor
+from darkwater_escape.darkwater_escape import dw_Controller, dw_Servo, dw_Motor
 
 dw = dw_PWMCONTROL( addr=0x61 )
 s = dw.getSERVO(1)
